1d2ddf7851a6017e5a8979fd7c2f727af9f6f21b
Kesava Rao K V <kvkeshavaarao@gmail.com>
2017-08-09 15:37:29 +0530

    Secon round of locally made changes 2017-08-09 15:40

--- server.js
+++ server.js
@@ -21,6 +21,22 @@ app.get('/ui/SC_ICON.ico', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'SC_ICON.ico'));
 });
 
+app.get('/ui/kvkr01.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'kvkr01.jpg'));
+});
+
+app.get('/ui/academics.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'academics.html'));
+});
+
+app.get('/ui/industry.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'industry.html'));
+});
+
+app.get('/ui/training.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'training.html'));
+});
+
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
